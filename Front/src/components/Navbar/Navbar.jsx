42a483635d01a856d8957b7bb5616cce66418b6e
OsvaldoRamirez97 <osvaldo97@live.com.ar>
2025-01-15 00:04:53 -0300

    Card and navBar

--- Front/src/components/Navbar/Navbar.jsx
+++ Front/src/components/Navbar/Navbar.jsx
@@ -4,12 +4,24 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav>
+    <nav className="navBar">
+      <Link to="/"><img src='/logo.jpeg' alt="Logo" className="logo"/></Link>
       <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/login">Login</Link></li>
-        <li><Link to="/about">About</Link></li>
+        <li><Link to="/about">About Us</Link></li>
+        <li><Link to="/join">How to Join</Link></li>
+        <li><Link to="/costs">Costs</Link></li>
+        <li><Link to="/benefits">Benefits</Link></li>
+        <li><Link to="/contact">Contact Us</Link></li>
       </ul>
+      <div>
+        <Link to="/sign" type="button" class="btn btn-primary">
+          SIGN UP FREE
+        </Link>
+        <Link to="/login" type="button" class="btn btn-primary">
+          <i class="bi bi-person-fill"></i>
+          LOGIN
+        </Link>
+      </div>
     </nav>
   );
 };
