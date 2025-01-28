21f8de1387f68c145b65eb541ef2e9c87f917153
OsvaldoRamirez97 <osvaldo97@live.com.ar>
2025-01-28 20:24:54 -0300

    Cambiando Banner

--- Front/src/pages/Home/Home.jsx
+++ Front/src/pages/Home/Home.jsx
@@ -5,6 +5,7 @@ import CardExperiences from "../../components/Card/CardExperiences";
 const Home = () => {
   return (
     <>
+      <video src="https://videos.pexels.com/video-files/3209571/3209571-uhd_2560_1440_25fps.mp4" autoplay="true" muted="true" loop="true"></video>
       <header className="header">
         <h1>EASY VOLUNTEERING ABROAD</h1>
         <p>Explore 1,800+ proyects abroad & find your best volunteer program</p>
