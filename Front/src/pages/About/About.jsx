62798ba62619d82e8a7fb08a07d59077c890be92
mangelicapj <mangelica.0894@gmail.com>
2025-01-27 21:41:50 -0500

    about

--- Front/src/pages/About/About.jsx
+++ Front/src/pages/About/About.jsx
@@ -3,24 +3,45 @@ import styles from './About.css';
 import CardTeam from './CardTeam/CardTeam';
 
 function About() {
- 
   const teamMembers = [
     {
       id: 1,
       name: 'Maria Angelica Palomino Jaramillo',
-      image: 'https://avatars.githubusercontent.com/u/128256723?v=4',
-      banner: 'https://media.licdn.com/dms/image/D4E16AQFu0Fl-BB0t3Q/profile-displaybackgroundimage-shrink_350_1400/0/1699049023012?e=1711584000&v=beta&t=67TvenofJBr9utHTO5qgu2Qls4yOwys0h-hk3Dvn_LM',
-      description: 'Full Stack Developer at ANAMI',
+      image: '/Marian.jpg',
+      description: 'Full Stack Developer at AMANI',
       linkedin: 'https://www.linkedin.com/in/mangelicapj/',
       github: 'https://github.com/mangelicapj',
     },
     {
       id: 2,
+      name: 'Osvaldo Ramirez',
+      image: 'https://media.licdn.com/dms/image/v2/D4D03AQHRsMerP0aNZA/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1718404573294?e=1743638400&v=beta&t=Rk8rw1krI5EoQ_8cBpeWxirOD2eOUKVmwLcq_hd3k7o',
+      description: 'Full Stack Developer at AMANI',
+      linkedin: 'https://www.linkedin.com/in/osvaldo-g-ramirez/',
+      github: 'https://github.com/osvaldoRamirez97/',
+    },
+    {
+      id: 3,
       name: 'Maximiliano Testa',
       image: 'https://ca.slack-edge.com/T02KS88FB0E-U087STAK4KA-be69c6de3c70-512',
-      banner: 'https://media.licdn.com/dms/image/D4E16AQFu0Fl-BB0t3Q/profile-displaybackgroundimage-shrink_350_1400/0/1699049023012?e=1711584000&v=beta&t=67TvenofJBr9utHTO5qgu2Qls4yOwys0h-hk3Dvn_LM',
-      description: 'Full Stack Developer at ANAMI',
-      linkedin: '',
+      description: 'Full Stack Developer at AMANI',
+      linkedin: 'https://www.linkedin.com/in/maximiliano-javier-testa-9b6631106/',
+      github: 'https://github.com/Maxi-17',
+    },
+    {
+      id: 4,
+      name: 'Alejandro Reyes',
+      image: 'https://ca.slack-edge.com/T02KS88FB0E-U079FF3DH8C-g6ea6d2ee80d-512',
+      description: 'Software QA Engineer at AMANI',
+      linkedin: 'https://www.linkedin.com/in/alejandro-reyes-software-qa-engineer/',
+      github: '',
+    },
+    {
+      id: 5,
+      name: 'Diego Guzmán',
+      image: 'https://media.licdn.com/dms/image/v2/C4D03AQGEpJ1WF9ALQw/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1618757947979?e=1743638400&v=beta&t=zU7xn3278mNc3EnHYOW5u92FyAhmF1xM6re7gu7upic',
+      description: 'Backend at AMANI',
+      linkedin: 'https://www.linkedin.com/in/diego-guzman-cerveux/',
       github: '',
     },
   ];
@@ -31,9 +52,11 @@ function About() {
         <Row className="g-0">
           <Col md={8} className="mx-auto">
             <Card.Body className={styles.textContainer}>
-              <Card.Title className={styles.text}>¡AMANI: PEACE IN ACTION, UNITY IN MOTION!</Card.Title>
+              <Card.Title className={styles.text}>
+                ¡AMANI: PEACE IN ACTION, UNITY IN MOTION!
+              </Card.Title>
               <Card.Text className={styles.text}>
-                Join AMANI and make a real impact! Discover volunteer opportunities that connect you to sustainable communities, eco-friendly projects, and life-changing experiences. Together, we create peace and unity through action. Start your journey today and be the change the world needs!
+                Join AMANI and make a real impact! Discover volunteer opportunities that connect you to sustainable communities, eco-friendly projects, and life-changing experiences. Together, we create peace and unity through action. Start your journey today and be the change the world needs! As junior developers, we came together with the goal of creating more than just a website. We wanted to build a purposeful platform that would connect individuals eager to do good with projects in need of support. Thus, AMANI was born, a name that means "peace" in Swahili, reflecting our vision to create a positive impact in the world. What began as a small dream quickly evolved into a project with a profound social purpose. We decided to use React to build the frontend, aiming for a simple yet attractive interface that would allow users to explore volunteering opportunities based on their interests and location. We incorporated features such as country filters and the creation of secure volunteer profiles, making AMANI not only functional but also user-friendly. Each informational section, from "How to Join" to "Benefits," was designed to be clear and direct, ensuring the platform’s purpose was always at the forefront. AMANI became more than just an ecommerce platform—it evolved into a real connection tool. For us as developers, it’s been more than just a technical project; it’s been the opportunity to create something with a tangible impact on the world. AMANI is in its early stages and growing, initially part of the No Country project but with a much greater purpose.
               </Card.Text>
             </Card.Body>
           </Col>
--- Front/src/pages/About/CardTeam/CardTeam.jsx
+++ Front/src/pages/About/CardTeam/CardTeam.jsx
@@ -1,23 +1,27 @@
 import { Card } from 'react-bootstrap';
-import styles from './CardTeam.css';
+import './CardTeam.css';
 
 function CardTeam({ name, image, description, linkedin, github }) {
   return (
-    <Card className={styles.teamCard}>
-      <div className={styles.imageContainer}>
-        <Card.Img variant="top" src={image} alt={`Photo of ${name}`} />
+    <Card className="teamCard">
+      <div className="image-container">
+        <img src={image} alt={`Photo of ${name}`} />
       </div>
       <Card.Body>
-        <Card.Title>{name}</Card.Title>
-        <Card.Text>{description}</Card.Text>
+        <Card.Title className="card-title">{name}</Card.Title>
+        <Card.Text className="card-text">{description}</Card.Text>
       </Card.Body>
-      <Card.Body className={styles.socialLinks}>
-        <Card.Link className="text-decoration-none" href={linkedin} target="_blank" rel="noopener noreferrer">
-          LinkedIn
-        </Card.Link>
-        <Card.Link className="text-decoration-none" href={github} target="_blank" rel="noopener noreferrer">
-          GitHub
-        </Card.Link>
+      <Card.Body className="social-links">
+        {linkedin && (
+          <Card.Link href={linkedin} target="_blank" rel="noopener noreferrer">
+            LinkedIn
+          </Card.Link>
+        )}
+        {github && (
+          <Card.Link href={github} target="_blank" rel="noopener noreferrer">
+            GitHub
+          </Card.Link>
+        )}
       </Card.Body>
     </Card>
   );
